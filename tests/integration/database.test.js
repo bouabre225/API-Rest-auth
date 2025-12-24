03d4498378e7103cbff05987348eb3c28665c0b1
Florent BOUDZOUMOU <florentboudz@gmail.com>
2025-12-24 15:05:46 +0100

    Test: Test e connection passed

--- tests/integration/database.test.js
+++ tests/integration/database.test.js
@@ -1,1 +1,23 @@
 import {test} from 'node:test';
+import assert from 'node:assert';
+import Database from 'better-sqlite3';
+import * as path from "node:path";
+import {describe} from "zod";
+
+const linkDB = path.resolve(process.cwd(), 'test.db');
+const db = new Database(linkDB);
+function testConnection(){
+    try {
+        db.prepare('SELECT 1').get();
+        return true;
+    } catch (err){
+        return false;
+    }
+}
+describe('Database configuration', ()=> {
+    test('should connect successfully', ()=>{
+        const isConnected = testConnection();
+        assert.strictEqual(isConnected, true);
+    })
+})
+
