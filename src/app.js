45f6b1cb85fbe60d262fa5fd6f51ca6e7de65904
Florent BOUDZOUMOU <florentboudz@gmail.com>
2025-12-24 01:24:13 +0100

    Fix: Correction de la structure, deplacement de la configuration dans app.js, pour permettre l'import de app dans les fichiers des tests

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+import { httpLogger } from "#lib/logger";
+import { errorHandler } from "#middlewares/error-handler";
+import { notFoundHandler } from "#middlewares/not-found";
+// Les routes seront importez ici
+
+// import userRoutes from "#routes/user.routes";
+
+const app = express();
+
+// Middlewares
+app.use(helmet());
+app.use(cors());
+app.use(httpLogger);
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "API Express opérationnelle" });
+});
+
+// app.use("/api/users", userRoutes);
+
+// Handlers
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+export default app; // CRITIQUE : On exporte l'objet sans le démarrer
