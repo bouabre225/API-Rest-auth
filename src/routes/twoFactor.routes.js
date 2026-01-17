170e065a2bdb30853870df42a3ad85532baa5f77
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2026-01-17 01:17:57 +0100

    fead: ajout de asynchandler

--- src/routes/twoFactor.routes.js
+++ src/routes/twoFactor.routes.js
@@ -2,10 +2,11 @@ import express from 'express';
 const router = express.Router();
 import {TwoFactorController} from '#controllers/twoFactorController';
 import {authMiddleware} from '#middlewares/auth.middleware';
+import {asyncHandler} from '#lib/async-handler';
 
-router.post('/enable', authMiddleware, TwoFactorController.enable);
-router.post('/confirm', authMiddleware, TwoFactorController.confirm);
-router.post('/disable', authMiddleware, TwoFactorController.disable);
-router.post('/verify', authMiddleware, TwoFactorController.verify);
+router.post('/enable', authMiddleware, asyncHandler(TwoFactorController.enable));
+router.post('/confirm', authMiddleware, asyncHandler(TwoFactorController.confirm));
+router.post('/disable', authMiddleware, asyncHandler(TwoFactorController.disable));
+router.post('/verify', authMiddleware, asyncHandler(TwoFactorController.verify));
 
 export default router;
